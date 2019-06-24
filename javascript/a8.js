bfde39e4ddb6c96c3959853caecfaad8dbab9543
mkay0961 <mkay0961@gmail.com>
2019-06-24 14:33:24 -0400

    Close need to improve time complexity

--- javascript/a8.js
+++ javascript/a8.js
@@ -1,10 +1,22 @@
  longestCommonPrefix = (strs) => {
-    let smallest = strs[0]
-    for(let str of strs){
-        if(str.length < smallest.length){
-            smallest = str
-        }
-    }
-
-    console.log(strs, smallest)
+   let smallest = strs[0]
+   for(let str of strs){
+       if(str.length < smallest.length){
+           smallest = str
+       }
+   }
+   let longestSub = ""
+   let string = ""
+   for(let i = 0; i < smallest.length; i++){
+       string += smallest[i]
+       console.log(string)
+       let cond = strs.map((el)=>{
+           return el.includes(string)
+       })
+       if(!cond.includes(false)){
+           longestSub = string
+       }
+   }
+   console.log(strs, longestSub)
+   return longestSub
 }
